191973b639177154d3d8250e17a12d75d90f6640
Bombshell2 <62397402+Bombshell2@users.noreply.github.com>
2023-02-15 19:39:32 -0500

    Update suggested command

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import sys
 if sys.version_info[0] < 3 or sys.version_info[1] < 4:
-    raise Exception("Must be using Python Version <= 3.4, please update at https://www.python.org/downloads/")
+    raise Exception("Must be using Python Version <= 3.4, please update at https://www.python.org/downloads/ OR run 'py -3 setup.py'")
 print("Installing Pynput...")
 os.system("pip install pynput")
 print("Installing PyAutoGui...")
